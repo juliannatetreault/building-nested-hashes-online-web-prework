4df37075e53ca5d7b8649b3ea137b6e9889f2583
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-16 12:03:56 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,8 +6,8 @@ def third_challenge
         age: "53"}
         },
       :matriarch => {
-        {:name => "Lady Monatgue", 
-        :age => "54"}
+        {name: "Lady Monatgue", 
+        age: "54"}
         },
       :hero => {
         {name: 
